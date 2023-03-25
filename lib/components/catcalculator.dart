ed59afc7b03e949b5c8428aba6171cc509ac736f
Sam Jav <ujaved007@gmail.com>
2023-03-25 14:50:31 +1100

    add form validations

--- lib/components/catcalculator.dart
+++ lib/components/catcalculator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CatCalculator extends StatefulWidget {
   const CatCalculator({super.key});
@@ -8,46 +9,71 @@ class CatCalculator extends StatefulWidget {
 }
 
 class _CatCalculatorState extends State<CatCalculator> {
-  calculate() {
-    debugPrint('calc');
+  final _yearsController = TextEditingController();
+  final _monthsController = TextEditingController();
+  bool _isFormValid = true;
+
+  validateForm() {
+    if (_formKey.currentState!.validate()) {
+      if (_yearsController.text == '' && _monthsController.text == '') {
+        setState(() {
+          _isFormValid = false;
+        });
+      } else {
+        setState(() {
+          _isFormValid = true;
+        });
+      }
+    }
   }
+
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(20),
       child: Form(
+        key: _formKey,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Text('How old is your cat?'),
-            Container(
-              padding: const EdgeInsets.only(top: 12),
-              child: Row(
-                children: const [
-                  Text("Years*"),
-                  SizedBox(width: 20),
-                  SizedBox(
-                    width: 50,
-                    child: TextField(
-                      keyboardType: TextInputType.number,
-                    ),
-                  ),
-                  SizedBox(width: 20),
-                  Text("Months"),
-                  SizedBox(width: 20),
-                  SizedBox(
-                    width: 50,
-                    child: TextField(
-                      keyboardType: TextInputType.number,
-                    ),
-                  ),
-                ],
+            const SizedBox(height: 12),
+            TextFormField(
+              controller: _yearsController,
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              decoration: const InputDecoration(
+                label: Text('Years'),
               ),
             ),
-            const SizedBox(height: 20),
+            TextFormField(
+              controller: _monthsController,
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              decoration: const InputDecoration(
+                label: Text('Months'),
+              ),
+              validator: (value) {
+                if (value != null && int.parse(value) > 11) {
+                  return 'Months value cannot be greater than 11';
+                } else {
+                  return null;
+                }
+              },
+            ),
+            !_isFormValid
+                ? Container(
+                    padding: const EdgeInsets.only(top: 20, bottom: 10),
+                    child: const Text(
+                      'Both months and years cannot be empty',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  )
+                : const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: calculate,
+              onPressed: validateForm,
               child: const Text('Calculate'),
             )
           ],
