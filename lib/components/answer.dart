04ef497661dd2571717b6d0c17286a21f1bcaa73
Sam Jav <ujaved007@gmail.com>
2023-03-25 16:18:50 +1100

    add cat calc functionality

---
+++ lib/components/answer.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final String petType;
+  final int years;
+  final int months;
+  const Answer(
+      {super.key,
+      required this.petType,
+      required this.years,
+      required this.months});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      width: double.infinity,
+      decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(16),
+          ),
+          color: Colors.black12),
+      child: Text(
+          'Your $petType is $years years ${months > 0 ? 'and $months months ' : ''}old.'),
+    );
+  }
+}
--- lib/components/catcalculator.dart
+++ lib/components/catcalculator.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'answer.dart';
+
 class CatCalculator extends StatefulWidget {
   const CatCalculator({super.key});
 
@@ -11,20 +13,61 @@ class CatCalculator extends StatefulWidget {
 class _CatCalculatorState extends State<CatCalculator> {
   final _yearsController = TextEditingController();
   final _monthsController = TextEditingController();
+  var _humanYears;
+  var _humanMonths;
   bool _isFormValid = true;
+  bool _showAnswer = false;
 
-  validateForm() {
+  void validateForm() {
     if (_formKey.currentState!.validate()) {
       if (_yearsController.text == '' && _monthsController.text == '') {
         setState(() {
           _isFormValid = false;
+          _showAnswer = false;
         });
       } else {
         setState(() {
           _isFormValid = true;
         });
+        catCalculator(
+            _monthsController.text == ''
+                ? 0
+                : int.parse(_monthsController.text),
+            _yearsController.text == '' ? 0 : int.parse(_yearsController.text));
       }
     }
+  }
+
+  void catCalculator(int months, int years) {
+    if (years == 0 && months > 0) {
+      int age = months * 15;
+      setState(() {
+        _humanYears = (age / 12).floor();
+        _humanMonths = age % 12;
+      });
+    } else if (years == 1) {
+      int age = months * 9;
+      setState(() {
+        _humanYears = 15 + (age / 12).floor();
+        _humanMonths = age % 12;
+      });
+    } else if (years == 2) {
+      int age = months * 4;
+      setState(() {
+        _humanYears = 24 + (age / 12).floor();
+        _humanMonths = age % 12;
+      });
+    } else if (years > 2) {
+      int age = months * 4;
+      setState(() {
+        _humanYears = 24 + (age / 12).floor() + ((years - 2) * 4);
+        _humanMonths = age % 12;
+      });
+    }
+    //show Answer widget
+    setState(() {
+      _showAnswer = true;
+    });
   }
 
   final _formKey = GlobalKey<FormState>();
@@ -47,6 +90,11 @@ class _CatCalculatorState extends State<CatCalculator> {
               decoration: const InputDecoration(
                 label: Text('Years'),
               ),
+              onChanged: (value) {
+                setState(() {
+                  _showAnswer = false;
+                });
+              },
             ),
             TextFormField(
               controller: _monthsController,
@@ -56,11 +104,18 @@ class _CatCalculatorState extends State<CatCalculator> {
                 label: Text('Months'),
               ),
               validator: (value) {
-                if (value != null && int.parse(value) > 11) {
+                if (value == null || value == '') {
+                  return null;
+                } else if (int.parse(value) > 11) {
                   return 'Months value cannot be greater than 11';
                 } else {
                   return null;
                 }
+              },
+              onChanged: (value) {
+                setState(() {
+                  _showAnswer = false;
+                });
               },
             ),
             !_isFormValid
@@ -75,7 +130,12 @@ class _CatCalculatorState extends State<CatCalculator> {
             ElevatedButton(
               onPressed: validateForm,
               child: const Text('Calculate'),
-            )
+            ),
+            const SizedBox(height: 20),
+            _showAnswer
+                ? Answer(
+                    petType: 'cat', years: _humanYears, months: _humanMonths)
+                : const SizedBox()
           ],
         ),
       ),
